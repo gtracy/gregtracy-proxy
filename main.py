44a3ef2aec3bf8dd9bcfb159a3c635ad49dda3bc
gtracy <gtracy@gmail.com>
2012-11-23 11:31:19 -0600

    ingore requests for /post/delete and strip off trailing slashes on all slugs

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from google.appengine.ext.webapp import template
 
 import blog_slugs
 
+BLOG_HOME = 'http://blog.gregtracy.com'
+
 # Data model to store request misses
 class Miss(db.Model):
   request = db.StringProperty()
@@ -27,20 +29,30 @@ def create_or_update(request):
 class MainHandler(webapp.RequestHandler):
     def get(self,slug=None):
       if( slug is not None and slug is not '' ):
-        slugs = blog_slugs.slugs.keys()
-        if( slug in slugs ):
-            new_slug = blog_slugs.slugs[slug]['new_slug']
-            post_date = blog_slugs.slugs[slug]['date']
-            if( new_slug == '' ):
-              new_slug = slug + '.html'
+          # strip off trailing slashes
+          slug = slug.strip('/')
+          
+          # ignore requests for /posts/destroy
+          if( slug.find('posts/destroy') >= 0 ):
+              self.redirect(BLOG_HOME)
+              return
 
-            self.redirect('http://blog.gregtracy.com%s/%s' % (post_date,new_slug))
-        else:
-            logging.debug('missed lookup for %s' % slug)
-            create_or_update(slug)
-            self.redirect('http://blog.gregtracy.com')
+          # search for the slug in our lookup table
+          slugs = blog_slugs.slugs.keys()
+          if( slug in slugs ):
+              new_slug = blog_slugs.slugs[slug]['new_slug']
+              post_date = blog_slugs.slugs[slug]['date']
+              if( new_slug == '' ):
+                  new_slug = slug + '.html'
+
+              self.redirect('%s%s/%s' % (BLOG_HOME,post_date,new_slug))
+          else:
+              logging.debug('missed lookup for %s' % slug)
+              create_or_update(slug)
+              self.redirect(BLOG_HOME)
+
       else:
-        self.redirect('http://blog.gregtracy.com')
+          self.redirect(BLOG_HOME)
 
       return      
 
